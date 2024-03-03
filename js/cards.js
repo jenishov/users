51dc4a424001d8888627b9f6a30886b12e27ae91
jenishov <adilet.jenishov@mail.ru>
2024-03-03 22:04:47 +0600

    test

---
+++ js/cards.js
@@ -0,0 +1,51 @@
+let catalogRow = document.querySelector('.row');
+ let search = document.querySelector('.header__search')
+
+
+
+    const getAllCards =()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((data) =>{
+                data.filter((el)=>{
+                    if (el.name.toUpperCase()){
+
+                    }
+                }).forEach((item)=>{
+                    catalogRow.innerHTML +=`
+
+            <div class="row__card">
+            
+                <img class="row__img" src="../img/user.png" alt="">
+                <div class="row__desc">
+                <h2 class="row__title">${item.name}</h2>
+                <p class="row__email">${item.email}</p>
+                <p class="row__phone">tel +${item.phone}</p>
+                </div>
+                
+            </div>
+            
+            `
+
+
+                })
+            })
+
+    }
+
+
+
+    getAllCards()
+
+
+
+
+
+
+
+
+
+
+
+
+
